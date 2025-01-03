4cff2abb3c0f5b36cd0eb6bfbbdcfc4a5b427074
Nazik <shmnazik79@gmail.com>
2025-01-04 00:37:33 +0530

    Header Styles added using screen options of stack navigator and added the app title for the header

--- UOV_Student_Care/App.js
+++ UOV_Student_Care/App.js
@@ -1,20 +1,34 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
+const appTitle = "UOV Student Care";
+
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#70116d" },
+            headerTitleStyle: { color: "#fff" },
+            headerTitleAlign: "center",
+          }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ title: appTitle }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: appTitle }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
