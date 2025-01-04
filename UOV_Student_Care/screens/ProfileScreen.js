728f171e053c3a4bb0943d77183bb73f0582637b
Nazik <shmnazik79@gmail.com>
2025-01-04 23:36:54 +0530

    Tab Navigation done in HomeScreen and banner added to ProfileScreen

--- UOV_Student_Care/screens/ProfileScreen.js
+++ UOV_Student_Care/screens/ProfileScreen.js
@@ -1,12 +1,26 @@
-import { View, Text } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
+import banner from "../assets/uov_banner.png";
 import React from "react";
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <>
+      <View style={styles.bannerContainer}>
+        <Image source={banner} style={styles.image} resizeMode="contain" />
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  bannerContainer: {
+    alignItems: "center",
+  },
+  image: {
+    width: 350,
+    borderRadius: 10,
+    marginTop: -25,
+  },
+});
 
 export default ProfileScreen;
